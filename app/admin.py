b13cccf6bb7b1b4bc48f8b4f92c37fa28cc190f4
aliyarahman <aliya.rahman@yahoo.com>
2015-01-02 22:44:24 -0500

    Updated model registration in admin view

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from app.models import Recommendation, Evaluation, Applicant, Evaluator, Recommender, Staff
 
 # Register your models here.
+admin.site.register(Applicant)
+admin.site.register(Recommender)
+admin.site.register(Evaluator)
+admin.site.register(Staff)
+admin.site.register(Evaluation)
+admin.site.register(Recommendation)
